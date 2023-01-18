63bc043539616c3786ae183e2c37d1b900758bec
Emiliano F Rodriguez <emilianofederico.mmo@outlook.com>
2023-01-17 22:37:45 -0300

    me gusta mas que salte con el espacio xd

--- JS/player.js
+++ JS/player.js
@@ -4,7 +4,7 @@ const player = document.getElementById("player");
 
 document.addEventListener('keydown', function (tecla) {
     console.log(tecla.key);
-    if (tecla.key == 'ArrowUp'){    
+    if (tecla.key == ' '){    
     playerJump(); }
 });
 
